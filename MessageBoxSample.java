ed8cfc514c688643fde29aa175fd66ff3b6b21a0
SancLunatic <sanclunatic@aim.com>
2014-02-12 21:37:41 -0800

    Initial Upload
    Displaying some of my small works as displayed on my website.

---
+++ MessageBoxSample.java
@@ -0,0 +1,36 @@
+﻿import java.awt.*;
+import java.awt.event.*;
+
+public class MessageBox extends Dialog implements ActionListener
+{
+ private String result;
+ private Button OKButton;
+
+ public MessageBox(Frame frame, String title, String messageString)
+ {
+  super(frame, title, true);
+
+  Rectangle bounds = frame.getBounds();
+  setBackground(Color.white);
+  setLocation(bounds.x+bounds.width/3, bounds.y+bounds.height/3);
+
+  Panel messagePane = new Panel();
+  Label message = new Label(messageString);
+  messagePane.add(message);
+  add(messagePane, BorderLayout.CENTER);
+
+  Panel buttonPane = new Panel();
+  OKButton = new Button("  OK  ");
+  buttonPane.add(OKButton);
+  add(buttonPane, BorderLayout.SOUTH);
+
+  OKButton.addActionListener(this);
+
+  pack();
+ }
+
+ public void actionPerformed(ActionEvent e)
+ {
+  setVisible(false);
+ }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+﻿Message Box Sample
+------------------
+Java Application that creates a message box.
